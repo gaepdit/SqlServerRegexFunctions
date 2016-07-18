57f3383940c6ab629377ca8bdf590b0dc8bebd34
Doug Waldron <douglas.waldron@dnr.ga.gov>
2016-07-18 13:41:34 -0400

    Add project files.

---
+++ RegexFunctions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexFunctions
+{
+    public class Class1
+    {
+    }
+}
