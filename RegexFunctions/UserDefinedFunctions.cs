c5c6b3d567b61089b71c221411ddf00a3688c95e
Doug Waldron <douglas.waldron@dnr.ga.gov>
2016-07-18 13:51:58 -0400

    Initial implementation of Regex Functions

---
+++ RegexFunctions/UserDefinedFunctions.cs
@@ -0,0 +1,105 @@
+﻿using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Text.RegularExpressions;
+using System.Text;
+
+public partial class UserDefinedFunctions
+{
+
+    public static readonly RegexOptions Options = RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline;
+
+    /// <summary>
+    /// Indicates whether the regular expression finds a match in the input string.
+    /// </summary>
+    /// <param name="input">The string to search for a match.</param>
+    /// <param name="pattern">The regular expression pattern to match. </param>
+    /// <returns>true (1) if the regular expression finds a match; otherwise, false (0).</returns>
+    [SqlFunction]
+    public static SqlBoolean RegexMatch(SqlChars input, SqlString pattern)
+    {
+        Regex regex = new Regex(pattern.Value, Options);
+        return regex.IsMatch(new string(input.Value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <param name="pattern"></param>
+    /// <param name="replace"></param>
+    /// <returns></returns>
+    [SqlFunction]
+    public static SqlString RegexReplace(SqlString expression, SqlString pattern, SqlString replace)
+    {
+        if (expression.IsNull || pattern.IsNull || replace.IsNull)
+            return SqlString.Null;
+
+        Regex regex = new Regex(pattern.ToString());
+        return new SqlString(regex.Replace(expression.ToString(), replace.ToString()));
+    }
+    
+    /// <summary>
+    /// Returns the matching string. Results are separated by the delimeter string
+    /// </summary>
+    /// <param name="input">The string to search for a match.</param>
+    /// <param name="pattern">The regular expression pattern to match. </param>
+    /// <param name="matchDelimiter">The delimeter string</param>
+    /// <returns>The matching strings separated by the delimeter string.</returns>
+    [SqlFunction]
+    public static SqlString RegexSelectAll(SqlChars input, SqlString pattern, SqlString matchDelimiter)
+    {
+        Regex regex = new Regex(pattern.Value, Options);
+        Match results = regex.Match(new string(input.Value));
+
+        StringBuilder sb = new StringBuilder();
+        while (results.Success)
+        {
+            sb.Append(results.Value);
+
+            results = results.NextMatch();
+
+            // separate the results with newline|newline
+            if (results.Success)
+            {
+                sb.Append(matchDelimiter.Value);
+            }
+        }
+
+        return new SqlString(sb.ToString());
+
+    }
+
+    /// <summary>
+    /// Returns the matching string. Only the nth match is returned
+    /// </summary>
+    /// <param name="input">The string to search for a match.</param>
+    /// <param name="pattern">The regular expression pattern to match. </param>
+    /// <param name="matchIndex">The index of the result to return</param>
+    /// <returns>The matching string (only the nth result)</returns>
+    [SqlFunction]
+    public static SqlString RegexSelectOne(SqlChars input, SqlString pattern, SqlInt32 matchIndex)
+    {
+        Regex regex = new Regex(pattern.Value, Options);
+        Match results = regex.Match(new string(input.Value));
+
+        string resultStr = "";
+        int index = 0;
+
+        while (results.Success)
+        {
+            if (index == matchIndex)
+            {
+                resultStr = results.Value.ToString();
+            }
+
+            results = results.NextMatch();
+            index++;
+
+        }
+
+        return new SqlString(resultStr);
+
+    }
+
+};
+
---
+++ SQL Server Objects/UserDefinedFunctions.sql
@@ -0,0 +1,71 @@
+SET ANSI_NULLS OFF;
+GO
+SET QUOTED_IDENTIFIER OFF;
+GO
+
+IF OBJECT_ID('dbo.RegexMatch') IS NOT NULL
+    DROP FUNCTION dbo.RegexMatch;
+GO
+
+CREATE FUNCTION dbo.RegexMatch
+(
+                @input   nvarchar(max)
+              , @pattern nvarchar(max)
+)
+RETURNS bit
+WITH EXECUTE AS CALLER,
+     RETURNS NULL ON NULL INPUT
+AS EXTERNAL NAME
+RegexFunctions.UserDefinedFunctions.RegexMatch;
+GO
+
+IF OBJECT_ID('dbo.RegexReplace') IS NOT NULL
+    DROP FUNCTION dbo.RegexReplace;
+GO
+
+CREATE FUNCTION dbo.RegexReplace
+(
+                @expression nvarchar(max)
+              , @pattern    nvarchar(max)
+              , @replace    nvarchar(max)
+)
+RETURNS nvarchar(max)
+WITH EXECUTE AS CALLER,
+     RETURNS NULL ON NULL INPUT
+AS EXTERNAL NAME
+RegexFunctions.UserDefinedFunctions.RegexReplace;
+GO
+
+IF OBJECT_ID('dbo.RegexSelectAll') IS NOT NULL
+    DROP FUNCTION dbo.RegexSelectAll;
+GO
+
+CREATE FUNCTION dbo.RegexSelectAll
+(
+                @input          nvarchar(max)
+              , @pattern        nvarchar(max)
+              , @matchDelimiter nvarchar(max)
+)
+RETURNS nvarchar(max)
+WITH EXECUTE AS CALLER,
+     RETURNS NULL ON NULL INPUT
+AS EXTERNAL NAME
+RegexFunctions.UserDefinedFunctions.RegexSelectAll;
+GO
+
+IF OBJECT_ID('dbo.RegexSelectOne') IS NOT NULL
+    DROP FUNCTION dbo.RegexSelectOne;
+GO
+
+CREATE FUNCTION dbo.RegexSelectOne
+(
+                @input      nvarchar(max)
+              , @pattern    nvarchar(max)
+              , @matchIndex int
+)
+RETURNS nvarchar(max)
+WITH EXECUTE AS CALLER,
+     RETURNS NULL ON NULL INPUT
+AS EXTERNAL NAME
+RegexFunctions.UserDefinedFunctions.RegexSelectOne;
+GO
